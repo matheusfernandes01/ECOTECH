ba1d01f0811907ffd1d7fe2130d5d396f01c88f7
unknown <matheusfernandes12000@gmail.com>
2023-07-06 17:27:02 -0300

    novas alterações

--- scripts/energias.js
+++ scripts/energias.js
@@ -1,6 +1,29 @@
-const imagens = document.querySelectorAll(".energias .container3 .imagens");
+// Energias
+const imagensEnergias = document.querySelectorAll(
+  ".energias .container3 .imagens"
+);
 
-imagens.forEach((imagem) => {
+imagensEnergias.forEach((imagem) => {
+  imagem.addEventListener("click", () => {
+    imagem.classList.toggle("active");
+  });
+});
+
+// Agricultura
+const imagensAgricultura = document.querySelectorAll(
+  ".agricultura .container4 .imagens2"
+);
+
+imagensAgricultura.forEach((imagem) => {
+  imagem.addEventListener("click", () => {
+    imagem.classList.toggle("active");
+  });
+});
+
+// Saúde
+const imagensSaude = document.querySelectorAll(".saude .container5 .imagens3");
+
+imagensSaude.forEach((imagem) => {
   imagem.addEventListener("click", () => {
     imagem.classList.toggle("active");
   });
